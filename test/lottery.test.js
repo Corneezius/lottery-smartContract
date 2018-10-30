dd022d679dbba87b296d1fa38f3945663adafcbd
Corneezius <cfr2116@columbia.edu>
2018-10-30 13:44:27 -0400

    update package.json to test properly

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const Web3 = new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider());
 
 const {interface,bytecode} = require("../compile");
 
@@ -14,4 +14,10 @@ beforeEach(async () => {
   lottery = await new web3.eth.Contract(JSON.parse(interface))
   .deploy({data: bytecode})
   .send({from:accounts[0], gas:"1000000"});
+});
+
+describe("Lottery Contract", () => {
+  it("deploys a contract", () => {
+    assert.ok(lottery.options.address);
+  });
 });
